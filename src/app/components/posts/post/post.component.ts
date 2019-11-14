bd855ed6d3f5d54d96bf9a9329663ffb895f4d46
Bezael <bezael_e@hotmail.com>
2019-11-14 20:48:41 +0100

    Post details layout

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  public post: {
+    id: string;
+    titlePost: string;
+    contentPost: string;
+    imagePost: string;
+  } = {
+    id: '1',
+    titlePost: ' Post One',
+    contentPost: 'Hola mundo ',
+    imagePost: 'https://picsum.photos/id/237/200/300'
+  };
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.post.id = this.route.snapshot.params.id;
   }
-
 }
