58020401389973fc7120db98b16ce818baaf3762
Bezael <bezael_e@hotmail.com>
2020-02-08 15:26:31 +0100

    New Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBI2cv4zz0GKu4aSg05a7LkWU-bFDer72E',
-    authDomain: 'ngblog-eab58.firebaseapp.com',
-    databaseURL: 'https://ngblog-eab58.firebaseio.com',
-    projectId: 'ngblog-eab58',
-    storageBucket: 'ngblog-eab58.appspot.com',
-    messagingSenderId: '1071362108248',
-    appId: '1:1071362108248:web:3f2b4665906646068d132d',
-    measurementId: 'G-E027XY5TMJ'
+    apiKey: 'XXXXXXXX',
+    authDomain: 'XXXXXX',
+    databaseURL: 'XXXXX',
+    projectId: 'XXXXXXXX',
+    storageBucket: 'XXXXXXXX.appspot.com',
+    messagingSenderId: 'XXXXXXXXX',
+    appId: '1:XXXXXXXXX:web:XXXXXXXXXXXX',
+    measurementId: 'X-XXXXXXXXXX'
   }
 };
 
